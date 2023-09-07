312606c1059751f4a80d0aa5fe5ac44264e4ed73
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-09-08 00:21:38 +0300

    comments

--- game_logic.py
+++ game_logic.py
@@ -22,3 +22,15 @@ def play_round(player_choice):
 def get_computer_choice():
     choices = ['rock', 'paper', 'scissors']  # List of possible choices
     return random.choice(choices)  # Return a randomly chosen choice for the computer
+
+# import random: This line imports the Python random module, which is used to generate random choices for the computer.
+
+# play_round(player_choice): This function simulates a single round of the game. It takes the player's choice as an argument.
+
+# choices = ['rock', 'paper', 'scissors']: This is a list containing the possible choices in the game.
+
+# computer_choice = random.choice(choices): This line randomly selects a choice from the choices list as the computer's choice.
+
+# The if and elif statements compare the player's choice to the computer's choice to determine the round's result. If the choices are the same, it's a tie. Otherwise, it checks if the player's choice beats the computer's choice based on the rules of the game.
+
+# get_computer_choice(): This function simply returns a random choice for the computer from the choices list.

a558ee182e801dfd7ec148d9ec0a32575ee4c9c3
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-09-08 00:29:25 +0300

    error sort

--- model.py
+++ model.py
@@ -40,3 +40,5 @@ Base.metadata.create_all(engine)
 # The Move class represents the 'moves' table and has columns for id, player_id (foreign key referencing a player), game_id (foreign key referencing a game), and choice to store the player's choice in the game.
 
 # Finally, it creates an SQLite database engine named 'game.db' and uses the create_all method to create the corresponding database tables based on the class definitions in the Base.metadata.
+
+# game_logic.py provides the core logic for playing a single round of the Rock, Paper, Scissors game and determining the result of the round.

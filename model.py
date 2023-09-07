e58fadc2fadc7d7ead14e9388445fd7d17cb9af8
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-09-08 00:22:23 +0300

    comments

--- model.py
+++ model.py
@@ -32,7 +32,11 @@ engine = create_engine('sqlite:///game.db')
 Base.metadata.create_all(engine)
 
 # Base is a base class provided by SQLAlchemy for declarative class definitions.
+
 # The Player class represents the 'players' table and has columns for id (primary key) and name.
+
 # The Game class represents the 'games' table and has a column for id (primary key).
+
 # The Move class represents the 'moves' table and has columns for id, player_id (foreign key referencing a player), game_id (foreign key referencing a game), and choice to store the player's choice in the game.
+
 # Finally, it creates an SQLite database engine named 'game.db' and uses the create_all method to create the corresponding database tables based on the class definitions in the Base.metadata.

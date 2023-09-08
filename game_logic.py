aa30f3ba07609afa48a0fae269f55128f87bfde8
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-09-08 03:07:37 +0300

    history

--- game_logic.py
+++ game_logic.py
@@ -1,27 +1,62 @@
 # game_logic.py
-import random  # Import the 'random' module to generate random choices
+import random
+from model import Player, Game, Move, engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 
-# Define a function to play a single round of the Rock, Paper, Scissors game
-def play_round(player_choice):
-    choices = ['rock', 'paper', 'scissors']  # List of possible choices
-    computer_choice = random.choice(choices)  # Randomly select the computer's choice
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    # Determine the result of the round based on player and computer choices
+def play_round(player_choice, player_name):
+    choices = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(choices)
+
     if player_choice == computer_choice:
-        return "It's a tie!"  # If both choices are the same, it's a tie
+        result = "It's a tie!"
     elif (
         (player_choice == 'rock' and computer_choice == 'scissors') or
         (player_choice == 'paper' and computer_choice == 'rock') or
         (player_choice == 'scissors' and computer_choice == 'paper')
     ):
-        return "You win!"  # Player wins if they beat the computer's choice
+        result = "You win!"
+        update_player_wins(player_name)  # Update player's wins
     else:
-        return "Computer wins!"  # Computer wins if none of the above conditions are met
+        result = "Computer wins!"
 
-# Define a function to get the computer's random choice
+    # Record the game result in the database
+    record_game(player_name, player_choice, computer_choice, result)
+
+    return result
+
 def get_computer_choice():
-    choices = ['rock', 'paper', 'scissors']  # List of possible choices
-    return random.choice(choices)  # Return a randomly chosen choice for the computer
+    choices = ['rock', 'paper', 'scissors']
+    return random.choice(choices)
+
+def update_player_wins(player_name):
+    player = session.query(Player).filter_by(name=player_name).first()
+    if player:
+        player.wins += 1
+        session.commit()
+
+def record_game(player_name, player_choice, computer_choice, result):
+    player = session.query(Player).filter_by(name=player_name).first()
+    if player:
+        game = Game(player=player, result=result)
+        session.add(game)
+        session.commit()
+
+def get_game_history(player_name):
+    player = session.query(Player).filter_by(name=player_name).first()
+    if player:
+        games = session.query(Game).filter_by(player=player).all()
+        return games
+    return []
+
+def get_player_wins(player_name):
+    player = session.query(Player).filter_by(name=player_name).first()
+    if player:
+        return player.wins
+    return 0
 
 # import random: This line imports the Python random module, which is used to generate random choices for the computer.
 

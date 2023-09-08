5c904377b6010b418f9cc2c0b4e05674dd9b9e06
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-09-08 03:21:24 +0300

    1st correct

--- cli.py
+++ cli.py
@@ -1,48 +1,53 @@
 # cli.py
-import argparse
-from game_logic import play_round, get_computer_choice, get_game_history, get_player_wins
-from model import Player, Game, engine
-from sqlalchemy.orm import sessionmaker
+# Import necessary modules and functions
+import argparse  # For parsing command-line arguments
+from game_logic import play_round, get_computer_choice  # Import game logic functions
+from model import Player, Game, Move, engine  # Import SQLAlchemy models and engine
+from sqlalchemy.orm import sessionmaker  # For creating a database session
 
+# Create a session object that will allow us to interact with the database
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Define the main function that will run when the script is executed
 def main():
+    # Create a command-line argument parser with a description
     parser = argparse.ArgumentParser(description="Rock, Paper, Scissors CLI Game")
+
+    # Define a command-line argument for the player's name
     parser.add_argument('player_name', type=str, help="Your name")
-    args = parser.parse_args()
+    args = parser.parse_args()  # Parse the command-line arguments
 
+    # Create or get the player from the database based on the provided name
     player = session.query(Player).filter_by(name=args.player_name).first()
-    if not player:
-        player = Player(name=args.player_name)
-        session.add(player)
-        session.commit()
 
+    if not player:
+        # If the player does not exist in the database, create a new player
+        player = Player(name=args.player_name)
+        session.add(player)  # Add the player to the session
+        session.commit()  # Commit the transaction to save the player in the database
+
+    # Start the game loop
     while True:
+        # Prompt the player to enter their choice
         player_choice = input("Enter your choice (rock/paper/scissors/q to quit): ").lower()
 
         if player_choice == 'q':
-            break
-        elif player_choice == 'history':
-            display_game_history(args.player_name)
+            break  # Exit the game loop if 'q' is entered
         elif player_choice not in ['rock', 'paper', 'scissors']:
-            print("Invalid choice. Please choose rock, paper, scissors, or q to quit.")
+            # Handle invalid input
+            print("Invalid choice. Please choose rock, paper, or scissors.")
         else:
-            result = play_round(player_choice, args.player_name)
-            print(f"Computer chose {get_computer_choice()}")
+            # Call the play_round function to determine the game result
+            result = play_round(player_choice)
+            # Get the computer's choice
+            computer_choice = get_computer_choice()
+            # Print the computer's choice and the game result
+            print(f"Computer chose {computer_choice}")
             print(result)
 
-def display_game_history(player_name):
-    games = get_game_history(player_name)
-    if not games:
-        print("No game history found.")
-    else:
-        print("\nGame History:")
-        for game in games:
-            print(f"Timestamp: {game.timestamp}, Result: {game.result}")
-        print(f"Total Wins: {get_player_wins(player_name)}")
-
+# Check if the script is being run directly (not imported as a module)
 if __name__ == "__main__":
-    main()
+    main()  # Call the main function to start the game
 
 # this codes set up a command-line interface (CLI) for the Rock, Paper, Scissors game using argparse for command-line argument parsing and SQLAlchemy for database operations. It allows players to enter their name, play the game, and stores player information in a database. The game loop continues until the player chooses to quit by entering 'q'.

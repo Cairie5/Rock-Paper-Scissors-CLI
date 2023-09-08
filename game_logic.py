c1a3be7c1e131609fecb326968525086dd8a44b4
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-09-08 03:23:08 +0300

    3rd

--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,27 @@
+# game_logic.py
+import random  # Import the 'random' module to generate random choices
+
+# Define a function to play a single round of the Rock, Paper, Scissors game
+def play_round(player_choice):
+    choices = ['rock', 'paper', 'scissors']  # List of possible choices
+    computer_choice = random.choice(choices)  # Randomly select the computer's choice
+
+    # Determine the result of the round based on player and computer choices
+    if player_choice == computer_choice:
+        return "It's a tie!"  # If both choices are the same, it's a tie
+    elif (
+        (player_choice == 'rock' and computer_choice == 'scissors') or
+        (player_choice == 'paper' and computer_choice == 'rock') or
+        (player_choice == 'scissors' and computer_choice == 'paper')
+    ):
+        return "You win!"  # Player wins if they beat the computer's choice
+    else:
+        return "Computer wins!"  # Computer wins if none of the above conditions are met
+
+# Define a function to get the computer's random choice
+def get_computer_choice():
+    choices = ['rock', 'paper', 'scissors']  # List of possible choices
+    return random.choice(choices)  # Return a randomly chosen choice for the computer
 
 # import random: This line imports the Python random module, which is used to generate random choices for the computer.
 
